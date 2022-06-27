1cf9ec7eb805077df3f96963db2d2a0826594c86
majed1458 <majedbenkhalifa99@gmail.com>
2022-06-27 05:38:10 +0200

    project intializing

---
+++ src/components/home/Home.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import './homeStyle.scss'
+
+const Home = () => {
+    return (
+        <div className='container'>
+            <div className="list-items">
+                <div className="item">
+                    <div className="content">
+                        <h1 className="title"> Hats</h1>
+                        <span className="describtion">shop Now</span>
+                    </div>
+                </div>
+                <div className="item">
+                    <div className="content">
+                        <h1 className="title"> Hats</h1>
+                        <span className="describtion">shop Now</span>
+                    </div>
+                </div><div className="item">
+                    <div className="content">
+                        <h1 className="title"> Hats</h1>
+                        <span className="describtion">shop Now</span>
+                    </div>
+                </div><div className="item">
+                    <div className="content">
+                        <h1 className="title"> Hats</h1>
+                        <span className="describtion">shop Now</span>
+                    </div>
+                </div><div className="item">
+                    <div className="content">
+                        <h1 className="title"> Hats</h1>
+                        <span className="describtion">shop Now</span>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    )
+}
+
+export default Home
